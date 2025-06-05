efc8c6567a468c21bdbed7acd91b4571b7088ae3
Salman-Khan <94063655+salman-khan03@users.noreply.github.com>
2025-06-05 17:19:14 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Calendar from '/components/Calendar.jsx';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>My Summer in Chicago</h1>
+      <h2>Discover the Windy City!</h2>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
